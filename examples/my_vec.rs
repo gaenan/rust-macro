2af5d770544a9c6270ab58e77041994a7c041559
“et007013” <et007013@gmail.com>
2025-04-09 22:01:03 +0800

    refactor: change crate to proc-macro

---
+++ examples/my_vec.rs
@@ -0,0 +1,41 @@
+use anyhow::Result;
+
+#[macro_export]
+macro_rules! my_vec {
+    () => {
+        Vec::new()
+    };
+    ($elem:expr; $n:expr) => {
+        std::vec::from_elem($elem, $n)
+    };
+    ($($x:expr),+ $(,)?) => {
+        {
+            <[_]>::into_vec(Box::new([$($x),+]))
+        }
+    };
+}
+
+fn main() -> Result<()> {
+    let v: Vec<i32> = my_vec![1;4];
+    let v1: Vec<i32> = my_vec![
+        "1".parse()?,
+        "2".parse()?,
+        "3".parse()?,
+        "4".parse()?,
+        "5".parse()?,
+        "6".parse()?,
+    ];
+    
+    
+    // let v = vec![1, 2, 3];
+
+    let v2 = <[_]>::into_vec(Box::new([1, 2, 3, 4]));
+
+    println!("first --> {:?}", v);
+
+    println!("second --> {:?}", v1);
+    
+    println!("third --> {:?}", v2);
+
+    Ok(())
+}
